fdfa760ae2742a50bacc7cdbb74c253e531dbb1c
HISOKA-404 <sm02professional@gmail.com>
2024-10-30 12:39:09 +0530

    Stock Web Application Complete
    Bugs are present -> Have to resolve them

--- StockWebApp.py
+++ StockWebApp.py
@@ -4,6 +4,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import six.moves
 
 # Stock Data 
 # Amazon: 1997 - 24/03/2022
@@ -33,8 +34,7 @@ def get_input():
     return start_date, end_date, stock_symbol
 
 # Get company name with help of symbol
-def company_name(symbol):
-    
+def get_company_name(symbol):
     match(symbol):
         case "AMZN":
             return "Amazon.com Inc"
@@ -50,8 +50,8 @@ def company_name(symbol):
             return "No Such Company Found"
 
 
-# Take dataframe input from user to retrieve that particular company data
-def get_company_data( symbol, start_date, end_date):
+# Take input from user to retrieve that particular company data
+def get_company_data(symbol, start_date, end_date):
     match(symbol.upper()):
         case "AMZN":
             df = pd.read_csv("D:/Programming/Projects/Stock Market Web Application/Stock_Data/AMZN.csv")
@@ -68,28 +68,53 @@ def get_company_data( symbol, start_date, end_date):
 
     # Get the date range to show stock data during that period
     # Will convert date to datetime format for consistency
-    start = pd.to_dateTime(start_date)
-    end = pd.to_dateTime(end_date)
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
     
     # Set the start and end indexes row both to 0
     start_row = 0
     end_row = 0
     
     # Start at the top of data_set Date column and check every row
-    
     # There might be a problem if exact user selected start date is not found
     # So to find out from which row onwards we need to start displaying data  
     # User selected start date is less than or equal to date present in data_set: From this row onwards we will start displaying stock data
     # So we are running this loop in order to check the next date where we can start displaying stock data to user 
     for i in range(len(df)):
-        if start<=pd.to_datetime(df["Date"][i]):
+        if start_date<=pd.to_datetime(df["Date"][i]):
             start_row = i # We can start displaying stock data from this row
             break
     
     # Now we have to do the same process for end date 
     # Where the user selected end date should be greater than or equal to given date
-    for j in range(len(df), 0, -1):
-        if end>=pd.to_datetime(df["Date"][j]):
-            end_row = j
+    for j in range(len(df)):
+        if end_date>=pd.to_datetime(df["Date"][len(df) - 1 - j]):
+            end_row = len(df) - 1 - j
             break
     
+    # Set the index to be date 
+    # We want to access the stock data in this date range "using this index 
+    df = df.set_index(pd.DatetimeIndex(df["Date"].values))
+    
+    return df.iloc[start_row:end_row+1, :] # return the data frame within this rows, 2nd parameter : describes return all the columns 
+
+# Get the user input 
+start_date, end_date, symbol = get_input()
+
+# Get the company data
+df = get_company_data(symbol, start_date, end_date)
+
+# Get the company name 
+company_name = get_company_name(symbol)
+
+# Display the close price
+st.header(company_name+" Close Price\n")
+st.line_chart(df["Close"])
+
+# Display the Volume
+st.header(company_name+" Volume\n")
+st.line_chart(df["Volume"])
+
+# Get statistics on the data
+st.header("Data Statistics")
+st.write(df.describe())
